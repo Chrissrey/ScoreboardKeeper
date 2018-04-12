f5ebb4c5d41b90096d43540a90ad20d38c45bde9
Chrissrey <calmaraz@mail.csuchico.edu>
2018-04-12 11:49:50 -0700

    update scoreBoard

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 //Author:Christopher Rey Almaraz
 #include <iostream>
 #include <vector>
-
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
 const int MAX_PERIODS = 10;
@@ -9,8 +10,11 @@ const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
 
+void printScoreboard(vector < vector <int> >);// creating a new funtion called Scoreboard
+int randomBetween( int,int);
+
 int main()
-{
+{ srand((int) time(0));
   int periods;
   int teams;
 
@@ -39,7 +43,19 @@ vector< vector<int> >scoreBoard;
                 scoreBoard[row][col]=0;
             }
        }
-     cout<<"SCOREBOARD"<<endl;
+       printScoreboard(scoreBoard);
+
+       for(int row=0; row<scoreBoard.size(); row++)
+       {
+        for(int col=0; col<scoreBoard[row].size(); col++)
+       {
+            scoreBoard[row][col]= randomBetween(5,5);
+       }
+       
+       }
+       printScoreboard(scoreBoard);
+
+ /*    cout<<"SCOREBOARD"<<endl;
 
      for(int row=0; row<teams; row++)
        {
@@ -49,7 +65,35 @@ vector< vector<int> >scoreBoard;
         cout<<endl;
        }
   
-//once created, display the scoreboard
+*///once created, display the scoreboard
   }
   return 0;
 }
+
+  void printScoreboard(vector< vector<int> > scoreBoard)
+
+{        cout<<"SCOREBOARD"<<endl;
+
+     for(int row=0; row<scoreBoard.size(); row++)
+       {
+        cout<< "Player "<< row+1<<": ";
+        for(int col=0; col<scoreBoard[row].size(); col++)
+       {
+            cout<< scoreBoard[row][col]<< "|";
+       }
+       cout<<endl;
+       }
+   
+}
+int randomBetween(int first,int second)
+{
+    if(first>second)
+    {
+        return second + rand()%(first-second+1);
+    }
+    else if(second>first) 
+    {
+        return first + rand()%(second-first+1);
+    }    
+}
+
